f278d09c88368bdb94b47b018d65b07c171c4e48
Raj <rajsingh704596@gmail.com>
2025-07-30 18:13:56 +0530

    #1,#2 Control methods used on both tweens and timelines and allow to play, pause, reverse or even speed up and many more properties

--- 1.BasicsAnimation - gsap.to() , gsap.from()/script.js
+++ 1.BasicsAnimation - gsap.to() , gsap.from()/script.js
@@ -1,5 +1,6 @@
 //! gsap.to() - initial to final position animation move
 //! gsap.from() - final to initial position animation move
+//! gsap.fromTo() - both state define initial and final
 
 //^ use a script tag or an external JS file    {Recommended: this line must be pass so when whole DOM Content Loaded then animation run}
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -51,4 +52,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     // repeat: -1,        // infinite time repeat
     yoyo: true, //animate and vice-versa animate together
   });
+
+  //$ click handlers for controlling the tween instance (gsap.to(),gsap.from(),gsap.fromTo())...
+  let tween = gsap.fromTo(
+    "#box5",
+    {
+      //initial state
+      x: 300,
+    },
+    {
+      // final state
+      x: 800,
+      y: -500,
+      duration: 4,
+      autoAlpha: 0,
+      repeat: Infinity,
+      paused: true,
+    }
+  );
+
+  document.querySelector("#play").onclick = () => tween.play();
+  document.querySelector("#pause").onclick = () => tween.pause();
+  document.querySelector("#resume").onclick = () => tween.resume();
+  document.querySelector("#reverse").onclick = () => tween.reverse();
+  document.querySelector("#restart").onclick = () => tween.restart();
 });
--- 2.GSAP Timeline/script.js
+++ 2.GSAP Timeline/script.js
@@ -20,50 +20,71 @@
 // });
 
 // so here one by one animation perform k liye manually type delay kar rhe hai for time adjust, but we can use GSAP timeline , where automatic element perform animation one by one synchronously
+//^ use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  //^ gsap code here!
 
-// let t1 = gsap.timeline();
+  let t1 = gsap.timeline({ paused: true }); //initially paused timeline
 
-// t1.to("#box1", {
-//   x: 1000,
-//   duration: 2,
-//   rotate: 360,
-// });
+  t1.to("#box1", {
+    x: 1000,
+    duration: 5,
+    rotate: 360,
+  });
 
-// t1.to("#box2", {
-//   x: 1000,
-//   duration: 2,
-//   rotate: 180,
-//   scale: 0.5,
-// });
+  t1.to("#box2", {
+    x: 1000,
+    duration: 5,
+    rotate: 180,
+    scale: 0.5,
+  });
 
-// t1.to("#box3", {
-//   x: 1000,
-//   duration: 2,
-//   rotate: 360,
-//   borderRadius: "50",
-// });
+  t1.to("#box3", {
+    x: 1000,
+    duration: 5,
+    rotate: 360,
+    borderRadius: "50%",
+  });
 
-// Other e.g.
+  // click handlers for controlling the timeline instance all element...
+  document.querySelector("#play").onclick = () => t1.play();
+  document.querySelector("#pause").onclick = () => t1.pause(); //pause
+  document.querySelector("#resume").onclick = () => t1.resume(); //resume (honors direction - reversed or not)
+  document.querySelector("#reverse").onclick = () => t1.reverse(); //reverse (always goes back towards the beginning)
+  document.querySelector("#restart").onclick = () => t1.restart();
+  document.querySelector("#seek").onclick = () => t1.seek(0.5); //jump to exactly 0.5 seconds into the tween
+  document.querySelector("#progress").onclick = () => t1.progress(0.25); //jump to exacty 1/4th into the tween 's progress:
+  document.querySelector("#timescale").onclick = () => t1.timeScale(0.5); //make the tween go half-speed
+  document.querySelector("#timescale2x").onclick = () => t1.timeScale(2); //make the tween go double-speed
+  document.querySelector("#kill").onclick = () => t1.kill(); //immediately kill the timeline and make it eligible for garbage collection
 
-var t2 = gsap.timeline();
+  // we can even chain control methods
+  // Play the timeline at double speed - in reverse.
+  document.querySelector("#chainmethodcontrol").onclick = () =>
+    t1.timeScale(2).reverse();
 
-t2.from("h2", {
-  y: -30,
-  opacity: 0,
-  duration: 1,
-  delay: 0.5,
-});
+  // Other e.g. timeline without control
 
-t2.from("li", {
-  y: -30,
-  opacity: 0,
-  duration: 1,
-  stagger: 0.3,
-});
+  var t2 = gsap.timeline();
 
-t2.from("h1", {
-  y: 20,
-  opacity: 0,
-  duration: 1,
-  scale: 0.2,
+  t2.from("h2", {
+    y: -30,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+  });
+
+  t2.from("li", {
+    y: -30,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+  });
+
+  t2.from("h1", {
+    y: 20,
+    opacity: 0,
+    duration: 1,
+    scale: 0.2,
+  });
 });
